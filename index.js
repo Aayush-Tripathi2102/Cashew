8e281fb9d34a8cf72b569af48895dceb4efb3969
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2025-09-18 16:00:05 +0530

    setup basic express app

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-import {ethers} from ethers;
+import express from "express";
+import cookieParser from 'cookie-parser';
+import { PORT } from "./config/env.js";
 
-const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Subscription Tracker API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
+
+export default app;
