21bd24f9fe63ef5b2d795c255cec6a2351a99304
Vedant62 <vedant.sharma0510@gmail.com>
2025-09-18 17:50:08 +0530

    registry functionality added

--- config/env.js
+++ config/env.js
@@ -2,4 +2,5 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const {PORT} = process.env
+export const {PORT, REGISTRY_ADDRESS, RPC_URL, PRIVATE_KEY} = process.env
+
